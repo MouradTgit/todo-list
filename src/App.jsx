645130d9a00d73a8d6b2a409a7ffad9eafb2a4d4
MouradTgit <mourad.toure@students.dominican.edu>
2026-05-13 14:33:14 -0700

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import AddTodo from "./components/AddTodo.jsx";
-import TodoList from "./components/TodoList.jsx";
-import "./App.css";
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+import FilterControls from "./components/FilterControls";
+import SortingControls from "./components/SortingControls"; // 1️⃣
+
+import './App.css'
 
 function App() {
   return (
     <div>
       <h1>Redux To-Do List</h1>
       <AddTodo />
+      <FilterControls />
+      <SortingControls /> {/* 2️⃣ */}
       <TodoList />
     </div>
   );
